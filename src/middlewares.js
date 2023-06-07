dab7cee4a47c20a6625860ce36979b319fc4c4be
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-07 19:36:17 +0700

    [FIX] middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,6 @@
+// import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
+
 function notFound(req, res, next) {
   res.status(404);
   const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
@@ -11,11 +14,24 @@ function errorHandler(err, req, res, next) {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
   });
 }
 
-module.exports = {
-  notFound,
-  errorHandler,
-};
+function verifyToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Access Token not found" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.email = decoded.email;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ message: "Invalid Token" });
+  }
+}
+
+module.exports = { notFound, errorHandler, verifyToken };
