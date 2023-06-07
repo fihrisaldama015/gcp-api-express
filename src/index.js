0733fa5a8f844d63f8ee4ed53cfaa99b3b8f9d79
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-06-07 19:35:59 +0700

    [FIX] main files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-const app = require("./app");
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+const router = require("./routes/index.js");
+const middlewares = require("./middlewares");
+
+const app = express();
+
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res
+    .json({
+      message: "Learn GCP App Engine",
+    })
+    .status(200);
+});
+
+app.use("/api/v1", router);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
